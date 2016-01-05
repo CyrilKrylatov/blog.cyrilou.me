2b5d1b8d12260fbefac4f9faef1228cc3736b7b9
Cyril K <cyril.krylatov+github@gmail.com>
2016-01-05 15:08:36 +0100

    Debug Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
             jekyllServe: {
                 command: 'jekyll serve'
             }
-        }
+        },
 
         watch: {
             jekyll: {
