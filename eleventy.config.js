646b8da661e03aeb3b61d83ba82e322fa464ecf3
Cyril Krylatov <cyril.krylatov+github@gmail.com>
2024-10-07 20:59:01 +0200

    Favicons

--- eleventy.config.js
+++ eleventy.config.js
@@ -20,7 +20,8 @@ export default async function(eleventyConfig) {
 		.addPassthroughCopy({
 			"./public/": "/"
 		})
-		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl");
+		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl")
+    .addPassthroughCopy({ "./images/favicons": "/" });
 
 	// Run Eleventy when these files change:
 	// https://www.11ty.dev/docs/watch-serve/#add-your-own-watch-targets
